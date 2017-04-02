3d56078f0b2ad8abd08eca4c8fe3764deea527b1
Chris Khoo <chris.khoo@gmail.com>
2017-04-01 21:29:08 -0700

    update to support extends and rulesDirectory

---
+++ lib/helpers.ts
@@ -0,0 +1,80 @@
+import * as fs from 'fs'
+import { flow } from 'lodash'
+import { extname } from 'path'
+
+const BUILT_IN_CONFIG = /^tslint:(.*)$/
+
+export interface TSLintConfig {
+  readonly extends?: string | string[],
+  readonly rulesDirectory?: string | string[],
+  readonly rules: { readonly [ruleName: string]: {} }
+}
+
+export function arrayize<T>(value?: T | T[]): T[] {
+  if (value === undefined) return []
+  if (Array.isArray(value)) return value
+  return [value]
+}
+
+export function dashify(string: string): string {
+    return string.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)
+}
+
+export function getDirRules(dir: string): ReadonlyArray<string> {
+  if (!fs.existsSync(dir)) return []
+
+  return fs.readdirSync(dir)
+  .filter(file => /Rule.js$/.test(file))
+  .map(file => file.substr(0, file.length - 'Rule.ts'.length))
+}
+
+export function getExtendRules(extend: string): ReadonlyArray<string> {
+  const matches = extend.match(BUILT_IN_CONFIG)
+  if (matches != null && matches.length > 0) return []
+
+  return flow(
+    () => require.resolve(extend),
+    readConfigFile,
+    config => config.rules,
+    Object.keys,
+  )()
+}
+
+export function readConfigFile(file: string): TSLintConfig {
+  if (extname(file) === '.json') {
+    return flow(
+      () => fs.readFileSync(file).toString().replace(/^\uFEFF/, ''),
+      stripComments,
+      JSON.parse,
+    )()
+  } else {
+    // tslint:disable-next-line:no-require-imports
+    const config = require(file)
+    delete require.cache[file]
+    return config
+  }
+}
+
+function stripComments(content: string): string {
+  // tslint:disable-next-line:max-line-length
+  const regexp: RegExp = /("(?:[^\\\"]*(?:\\.)?)*")|('(?:[^\\\']*(?:\\.)?)*')|(\/\*(?:\r?\n|.)*?\*\/)|(\/{2,}.*?(?:(?:\r?\n)|$))/g
+  const result = content.replace(regexp, (match, _m1: string, _m2: string, m3?: string, m4?: string) => {
+      // Only one of m1, m2, m3, m4 matches
+      if (m3 !== undefined) {
+          // A block comment. Replace with nothing
+          return ''
+      } else if (m4 !== undefined) {
+          // A line comment. If it ends in \r?\n then keep it.
+          const length = m4.length
+          if (length > 2 && m4[length - 1] === '\n') {
+              return m4[length - 2] === '\r' ? '\r\n' : '\n'
+          } else {
+              return ''
+          }
+      } else {
+          // We match a string
+          return match
+      }
+  })
+  return result
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,65 +1,14 @@
 import * as fs from 'fs'
-import { chain, difference, flow, uniq } from 'lodash'
+import { chain, difference } from 'lodash'
+import {
+  arrayize,
+  dashify,
+  getDirRules,
+  getExtendRules,
+  readConfigFile,
+  TSLintConfig,
+} from './helpers'
 
-const RULE_DIRECTORIES: ReadonlyArray<string> = [
-  'node_modules/tslint/lib/rules',
-  'node_modules/tslint-immutable/rules',
-  'node_modules/tslint-react/rules',
-]
-
-interface TSLintJSON {
-  readonly extends?: string | string[],
-  readonly rulesDirectory?: string | string[],
-  readonly rules: { readonly [ruleName: string]: {} }
-}
-
-function arrayize<T>(value: T | T[]): T[] {
-  if (Array.isArray(value)) return value
-  return [value]
-}
-
-function dashify(string: string): string {
-    return string.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`)
-}
-
-function getRules(dir: string): string[] {
-  if (!fs.existsSync(dir)) return []
-
-  return fs.readdirSync(dir)
-  .filter(file => /Rule.js$/.test(file))
-  .map(file => file.substr(0, file.length - 'Rule.ts'.length))
-}
-
-function getRuleDirectories({ rulesDirectory }: TSLintJSON): ReadonlyArray<string> {
-  if (rulesDirectory !== undefined) return uniq(RULE_DIRECTORIES.concat(arrayize(rulesDirectory)))
-  return RULE_DIRECTORIES
-}
-
-function stripComments(content: string): string {
-  // tslint:disable-next-line:max-line-length
-  const regexp: RegExp = /("(?:[^\\\"]*(?:\\.)?)*")|('(?:[^\\\']*(?:\\.)?)*')|(\/\*(?:\r?\n|.)*?\*\/)|(\/{2,}.*?(?:(?:\r?\n)|$))/g
-  const result = content.replace(regexp, (match, _m1: string, _m2: string, m3?: string, m4?: string) => {
-      // Only one of m1, m2, m3, m4 matches
-      if (m3 !== undefined) {
-          // A block comment. Replace with nothing
-          return ''
-      } else if (m4 !== undefined) {
-          // A line comment. If it ends in \r?\n then keep it.
-          const length = m4.length
-          if (length > 2 && m4[length - 1] === '\n') {
-              return m4[length - 2] === '\r' ? '\r\n' : '\n'
-          } else {
-              return ''
-          }
-      } else {
-          // We match a string
-          return match
-      }
-  })
-  return result
-}
-
-// main
 if (!fs.existsSync('tslint.json')) {
   console.error('Missing tslint.json')
   process.exit(-1)
@@ -69,21 +18,24 @@ if (!fs.existsSync('node_modules')) {
   process.exit(-1)
 }
 
-const tslintJSON: TSLintJSON = flow(
-  (fileName: string) => fs.readFileSync(fileName).toString().replace(/^\uFEFF/, ''),
-  stripComments,
-  // tslint:disable-next-line:no-unbound-method
-  JSON.parse,
-)('tslint.json')
+const TSLINT_RULE_DIRECTORY = 'node_modules/tslint/lib/rules'
 
-const myRules = Object.keys(tslintJSON.rules)
+const mainConfig: TSLintConfig = readConfigFile('tslint.json')
 
-const allRules = chain(getRuleDirectories(tslintJSON))
-.map(getRules)
+const myRules = Object.keys(mainConfig.rules)
+
+const dirRules = chain(arrayize(mainConfig.rulesDirectory).concat(TSLINT_RULE_DIRECTORY))
+.map(getDirRules)
 .flatten()
 .map(dashify)
-.uniq()
 .value()
+
+const extendRules = chain(arrayize(mainConfig.extends))
+.map(getExtendRules)
+.flatten()
+.value()
+
+const allRules = chain(dirRules).concat(extendRules).uniq().value()
 
 console.log('Unspecified rules in tslint.json')
 console.log('================================')
